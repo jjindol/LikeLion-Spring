e205caed7f656c293bf3565bbacc5f07b6e9e13a
jjindol <andy15948@naver.com>
2023-08-30 14:16:20 +0900

    230830 commit

--- src/main/java/com/example/demo/repository/EvaluationRepository.java
+++ src/main/java/com/example/demo/repository/EvaluationRepository.java
@@ -1,8 +1,6 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.Evaluation;
-import com.example.demo.entity.Likey;
-import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
